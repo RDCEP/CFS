eed3ca26c1fd5374a08e4b8a55a688b379897cb6
Neil Best <nbest@ci.uchicago.edu>
2013-02-19 14:30:05 -0600

    adjusted CFS URLs and switched to 30-day intervals

--- scripts/cfsArchive.R
+++ scripts/cfsArchive.R
@@ -1,79 +1,111 @@
 library( stringr, quietly= TRUE)
 library( doMC, quietly= TRUE)
+library( lubridate)
 
 registerDoMC( cores= multicore:::detectCores())
 ## options( internet.info= 0)
 
-noClobber <- FALSE
 
-baseUrl <- "ftp://nomads.ncdc.noaa.gov/CFSR/HP_time_series"
+## noClobber currently has no effect
+
+## TODO: implement forced overwrite for if( noClobber)
+
+noClobber <- TRUE
+
+baseUrl <- "http://nomads.ncdc.noaa.gov/modeldata/cmd_ts_9mon"
 
 cfsVars <- c( "dswsfc", "prate", "tmax", "tmin")
 
-cfsMonths <- seq(
-  from= ISOdatetime( 1979,  1, 1, 0, 0, 0, "GMT"),
-  to=   ISOdatetime( 2009, 12, 1, 0, 0, 0, "GMT"),
-  by= "1 month")
+## CFS archive does not account for leap days, so the sequence is
+## somewhat irregular
 
-cfsUrlsByVar <- function( cfsVar) {
+everyThirtyJulianDaysNoLeapDays <- function( year) {
+  interval <- c(
+    1, 30,
+    ifelse( leap_year( year), 31, 30),
+    rep( 30, times= 9))
+  days <- cumsum( interval)
+  dates <- as.POSIXct( paste( year, days), format= "%Y %j", tz= "GMT")
+  names( dates) <- sprintf( "%4d.%02d", year, days %/% 30 + 1)
+  dates
+}
+              
+cfsDates <- foreach(
+  year= 1981:2011,
+  .combine= c) %do% {
+    everyThirtyJulianDaysNoLeapDays( year)
+  }
+
+cfsDates <-
+  cfsDates[  cfsDates >= ISOdatetime( 1981,  12, 12, 0, 0, 0, "GMT")
+           & cfsDates <= ISOdatetime( 2011,   3, 27, 0, 0, 0, "GMT")]
+
+writeCfsUrls <- function( cfsVar, cfsDate, md5= FALSE) {
   cfsUrlFormat <- paste(
     baseUrl,
-    "%Y%m",
-    paste( cfsVar, "gdas", "%Y%m", "grb2", sep= "."),
+    "%Y", "%Y%m",
+    paste( cfsVar, "%Y%m%d%H", "time", "grb2", sep= "."),
     sep= "/")
-  strftime( cfsMonths, tz= "GMT", format= cfsUrlFormat)
+  cfsFileUrl <- strftime(
+    cfsDate, tz= "GMT",
+    format= cfsUrlFormat)
+  if( md5) {
+    paste( cfsFileUrl, "md5", sep= ".")
+  } else {
+    cfsFileUrl
+  }
 }
 
-cfsUrls <- as.vector( sapply( cfsVars, cfsUrlsByVar))
 
-cfsDests <- str_replace(
-  string= cfsUrls,
-  pattern= sprintf( "^%s", baseUrl),
-  replacement= "data/cfs")
+cfsUrls <-
+  foreach( cfsVar= cfsVars,
+          .combine= c) %:%
+  foreach( cfsDate= cfsDates,
+          .combine= c) %dopar% {
+    writeCfsUrls( cfsVar, cfsDate)}
 
-log <- foreach(
-  u= cfsUrls,
-  ## d= cfsDests,
-  .combine= c) %dopar%
-{
-  ## dir.create( dirname( d), recursive= TRUE)
-  ## if( file.exists( d) && noClobber) {
-  ##   sprintf( "%s *EXISTS*", d)
-  ## } else {
-  ## t <- try(
-    ## download.file(
-    ##   url= u,
-    ##   destfile= d,
-    ##   ## mode= "wb",
-    ##   method= "wget",
-    ##   extra= paste(
-    ##     "--retry-connrefused",
-    ##     "--password=nbest@ci.uchicago.edu"),          
-    ##   cacheOK= FALSE,
-    ##   quiet= FALSE),
-    ## silent= TRUE)
-  t <- system(
+cfsMd5Urls <-
+  foreach( cfsVar= cfsVars,
+          .combine= c) %:%
+  foreach( cfsDate= cfsDates,
+          .combine= c) %dopar% {
+    writeCfsUrls( cfsVar, cfsDate, md5= TRUE)}
+
+## cfsUrlNames <-
+##   foreach( cfsVar= cfsVars) %do% {
+##     paste( cfsVar, names( cfsDates), sep= ".")
+##   }
+
+## names( cfsUrls) <- cfsUrlNames
+
+writeCfsWgetCommands <- function( cfsUrls) {
+  paste(
+    "wget", "--progress=dot:mega", ## "--no-verbose",
+    "--retry-connrefused",
+    "--continue",
+    ## "--password=nbest@ci.uchicago.edu",
+    "--timestamping",
+    "--no-host-directories",
     paste(
-      "wget", ##  --no-verbose",
-      "--retry-connrefused",
-      "--continue",
-      "--password=nbest@ci.uchicago.edu",
-      "--timestamping",
-      "--no-host-directories",
-      paste(
-        "--directory-prefix=data/cfs",
-        str_match( u, "/([0-9]{6})/")[,2],
-        sep="/"),
-      u),
-    intern= TRUE)
-  ##   )
-  ## if( inherits( t, "try-error") ||  t != 0) {
-  ##   file.remove( d)
-  ##   sprintf( "%s *FAILED* %s", d, t)
-  ## } else {
-  sprintf( "%s\n%s", u, t)
-  ## }
+      "--directory-prefix=data/cfs",
+      str_match( cfsUrls, "/([0-9]{4})/")[,2],
+      sep="/"),
+    cfsUrls,
+    "2>&1")
 }
 
-cat( log, sep= "\n")
-warnings()
+## cfsWgetCommands <- c(
+##     writeCfsWgetCommands( cfsUrls),
+##     writeCfsWgetCommands( cfsMd5Urls))
+
+  
+foreach(
+  cfsWgetCommand= writeCfsWgetCommands( cfsUrls)) %dopar% {
+    cat( system( cfsWgetCommand, intern= TRUE), sep= "\n")
+  }
+
+
+## TODO: check the MD5 sums in the course of downloading and retry on
+## checksum failure
+
+## TODO: create symlinks to name by pseudo-months
