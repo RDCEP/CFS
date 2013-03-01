fc555cb03fec1b3a489ec4325b64937ec0b040d3
Neil Best <nbest@ci.uchicago.edu>
2013-03-01 10:35:31 -0600

    more variables, all runs, all hours, but loses destination directories

--- scripts/cfs2.R
+++ scripts/cfs2.R
@@ -3,77 +3,77 @@ library( XML)
 library( stringr)
 library( doMC)
 
-registerDoMC( cores= 4)
+## registerDoMC( cores= 4)
+registerDoMC( cores= multicore:::detectCores())
 
 baseUrl <- "http://nomads.ncep.noaa.gov/pub/data/nccf/com/cfs/prod/cfs"
 
-subDir <- "00/time_grib_01"
+## subDir <- "00/time_grib_01"
 
-cfsVars <- c( "prate", "tmax", "tmin", "dswsfc")
-
-dataPath <- "data/cfs2"
-
-## curl <- getCurlHandle()
-
-cfsListing <- htmlTreeParse( baseUrl, isURL= TRUE, useInternalNodes= TRUE)
-
-cfsHrefs <- as( unlist( cfsListing[ "//@href"]), "character")
-names( cfsHrefs) <- NULL
-
-cfsMtimes <-
-  strptime(
-    sapply(
-      unlist( cfsListing[ "//body/pre/text()"]),
-      function( x) str_trim( as( x, "character"))),
-    format= "%d-%b-%Y %H:%M    -")
-
-cfsDf <- 
-  data.frame( href=  cfsHrefs[ !is.na( cfsMtimes)],
-             mtime= cfsMtimes[ !is.na( cfsMtimes)],
-             stringsAsFactors= FALSE)
-
-## are any of these links for the 1st or 15th day?
-cfsDf <- with( cfsDf, cfsDf[ str_detect( href, "^cfs\\.[0-9]{6}(01|15)"),])
-
+date <- strftime( as.Date( "2013-02-27"), format= "%Y%m%d")
 
 dataUrls <-
+  with(
+    expand.grid(
+      base= baseUrl,
+      date= date,
+      run=  c( "01", "02", "03", "04"),
+      hour= c( "00", "06", "12", "18"),
+      var=  c( "prate", "tmax", "tmin", "dswsfc", "crain", "q2m", "wnd10m")),
+    paste(
+      base,
+      sprintf( "cfs.%s", date),
+      hour,
+      sprintf( "time_grib_%s", run),
+      sprintf(
+        "%s.%s.%s%s.daily.grb2",
+        var, run, date, hour), 
+      sep= "/"))
+  
+wgetCommands <-
   paste(
-    baseUrl, substr( cfsDf$href, 1, 12), subDir,
-    sprintf( "%s.01.%s00.daily.grb2",
-            cfsVars, substr( cfsDf$href, 5, 12)),
-    sep= "/")
+    "wget", "--progress=dot:mega", ## "--no-verbose",
+    "--retry-connrefused",
+    "--continue",
+    "--timestamping",
+    "--no-host-directories",
+    "--cut-dirs=7",
+    "--directory-prefix=data/cfs2",
+    dataUrls,
+    "2>&1")
 
-## mapply(
-##   download.file,
-##   dataUrls,
-##   paste( dataPath, basename( dataUrls), sep="/"))
-
-
-log <- foreach(
-  u= dataUrls,
-  d= paste( dataPath, basename( dataUrls), sep="/"),
-  .combine= c) %dopar%
-{
-  dir.create( dirname( d), recursive= TRUE)
-  if( file.exists( d) && noClobber) {
-    sprintf( "%s *EXISTS*", d)
-  } else {
-    t <- try(
-      download.file(
-        url= u,
-        destfile= d,
-        mode= "wb",
-        cacheOK= FALSE,
-        quiet= FALSE),
-      silent= TRUE)
-    if( inherits( t, "try-error") ||  t != 0) {
-      file.remove( d)
-      sprintf( "%s *FAILED* %s", d, t)
-    } else {
-      sprintf( "%s", d)
-    }
+foreach(
+  wgetCommand= wgetCommands) %dopar% {
+    cat( system( wgetCommand, intern= TRUE), "\n", sep= "\n")
   }
-}
 
-cat( log, sep= "\n")
+## log <- foreach(
+##   u= dataUrls,
+##   d= paste( dataPath, basename( dataUrls), sep="/"),
+##   .combine= c) %dopar%
+## {
+##   dir.create( dirname( d), recursive= TRUE)
+##   if( file.exists( d) && noClobber) {
+##     sprintf( "%s *EXISTS*", d)
+##   } else {
+##     t <- try(
+##       download.file(
+##         url= u,
+##         destfile= d,
+##         mode= "wb",
+##         cacheOK= FALSE,
+##         quiet= FALSE),
+##       silent= TRUE)
+##     if( inherits( t, "try-error") ||  t != 0) {
+##       file.remove( d)
+##       sprintf( "%s *FAILED* %s", d, t)
+##     } else {
+##       sprintf( "%s", d)
+##     }
+##   }
+## }
+
+## cat( log, sep= "\n")
+
+
 warnings()
