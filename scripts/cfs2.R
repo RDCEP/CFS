cc5662ff60fe38de9fe733f0448a857fd5ba7c9d
Neil Best <nbest@ci.uchicago.edu>
2013-03-01 13:47:43 -0600

    downloads previous six days' data and preserves directory structure

--- scripts/cfs2.R
+++ scripts/cfs2.R
@@ -10,70 +10,67 @@ baseUrl <- "http://nomads.ncep.noaa.gov/pub/data/nccf/com/cfs/prod/cfs"
 
 ## subDir <- "00/time_grib_01"
 
-date <- strftime( as.Date( "2013-02-27"), format= "%Y%m%d")
+cfsDates <-
+  seq(
+    from= Sys.Date() -1,
+    by= "-1 day",
+    length.out= 6)
+
+strftime( cfsDates, format= "cfs.%Y%m%d")
 
 dataUrls <-
   with(
     expand.grid(
       base= baseUrl,
-      date= date,
+      date= cfsDates,
       run=  c( "01", "02", "03", "04"),
       hour= c( "00", "06", "12", "18"),
-      var=  c( "prate", "tmax", "tmin", "dswsfc", "crain", "q2m", "wnd10m")),
+      var=  c(
+        "prate", "tmax", "tmin", "dswsfc",
+        "crain", "q2m", "wnd10m")),
     paste(
       base,
-      sprintf( "cfs.%s", date),
+      strftime( date, format= "cfs.%Y%m%d"),
       hour,
       sprintf( "time_grib_%s", run),
-      sprintf(
-        "%s.%s.%s%s.daily.grb2",
-        var, run, date, hour), 
+      paste(
+        var, run,
+        paste(
+          strftime(
+            date,
+            format= "%Y%m%d"),
+          sprintf( "%s.daily.grb2", hour),
+          sep= ""),
+        sep= "."), 
       sep= "/"))
   
 wgetCommands <-
   paste(
-    "wget", "--progress=dot:mega", ## "--no-verbose",
+    "wget",
+    "--recursive",
+    "--progress=dot:mega",
+    ## "--no-verbose",
     "--retry-connrefused",
     "--continue",
     "--timestamping",
     "--no-host-directories",
     "--cut-dirs=7",
-    "--directory-prefix=data/cfs2",
     dataUrls,
     "2>&1")
 
-foreach(
-  wgetCommand= wgetCommands) %dopar% {
-    cat( system( wgetCommand, intern= TRUE), "\n", sep= "\n")
-  }
+oldWd <- setwd( "data/cfs2")
 
-## log <- foreach(
-##   u= dataUrls,
-##   d= paste( dataPath, basename( dataUrls), sep="/"),
-##   .combine= c) %dopar%
-## {
-##   dir.create( dirname( d), recursive= TRUE)
-##   if( file.exists( d) && noClobber) {
-##     sprintf( "%s *EXISTS*", d)
-##   } else {
-##     t <- try(
-##       download.file(
-##         url= u,
-##         destfile= d,
-##         mode= "wb",
-##         cacheOK= FALSE,
-##         quiet= FALSE),
-##       silent= TRUE)
-##     if( inherits( t, "try-error") ||  t != 0) {
-##       file.remove( d)
-##       sprintf( "%s *FAILED* %s", d, t)
-##     } else {
-##       sprintf( "%s", d)
-##     }
-##   }
-## }
-
-## cat( log, sep= "\n")
+log <-
+  foreach(
+    wgetCommand= wgetCommands,
+    .combine= c) %dopar% {
+      c( system( wgetCommand, intern= TRUE), "\n")
+    }
 
 
-warnings()
+cat(
+  log,
+  warnings(),
+  sep= "\n")
+
+setwd( oldWd)
